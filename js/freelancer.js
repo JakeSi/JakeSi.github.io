26d09a756c6430c2c890a3847fb3b3fcdf4e847a
JAKE SI <jake.si@live.com>
2015-10-25 00:19:54 -0400

    added parallax

--- js/freelancer.js
+++ js/freelancer.js
@@ -40,3 +40,27 @@ $('.navbar-collapse ul li a').click(function() {
 function openMail(){
      window.location.href = "mailto:jake.si@live.com?";
 }
+//============================================================================================================
+//|         "https://www.script-tutorials.com/bootstrap-one-page-template-with-parallax-effect/"             |
+//============================================================================================================
+$(document).ready(function(){
+   // cache the window object
+   $window = $(window);
+ 
+   $('section[data-type="background"]').each(function(){
+     // declare the variable to affect the defined data-type
+     var $scroll = $(this);
+                     
+      $(window).scroll(function() {
+        // HTML5 proves useful for helping with creating JS functions!
+        // also, negative value because we're scrolling upwards                             
+        var yPos = -($window.scrollTop() / $scroll.data('speed')); 
+         
+        // background position
+        var coords = '50% '+ yPos + 'px';
+ 
+        // move the background
+        $scroll.css({ backgroundPosition: coords });    
+      }); // end window scroll
+   });  // end section function
+}); // close out script
